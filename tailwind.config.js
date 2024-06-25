85fbf2281b121c280f8b5c2bce1f6f9a3bd8833d
NatthapatYang <158507615+NatthapatYang@users.noreply.github.com>
2024-06-25 22:34:30 +0700

    Setup Tailwindcss (#3)
    * Setup and test tailwindcss
    
    * Fix coding style problem from P'mew comment

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./rfm_dashboard/templates/**/*.html"],
+    theme: {
+      extend: {
+      },
+    },
+    plugins: [],
+  }
