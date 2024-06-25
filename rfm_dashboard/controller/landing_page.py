2b145092c98ea4a67c205266f9e672e590173606
NatthapatYang <natthapatyang@gmail.com>
2024-06-25 22:23:26 +0700

    Setup and test tailwindcss

--- rfm_dashboard/controller/landing_page.py
+++ rfm_dashboard/controller/landing_page.py
@@ -6,4 +6,4 @@ bp = Blueprint('landing_page', __name__)
 @bp.route('/rfm',methods=['GET'])
 def index():
     rfm_data = get_rfm_data()
-    return render_template('index.html', rfm_data=rfm_data)
+    return render_template('landing/index.html', rfm_data=rfm_data)
