b090464b2bcac7bbc3a230f4e28c78e839194674
NatthapatYang <natthapatyang@gmail.com>
2024-06-27 15:43:22 +0700

    Update landing page, controller and models to show percent of bar chart on landing page

--- rfm_dashboard/controller/landing_page.py
+++ rfm_dashboard/controller/landing_page.py
@@ -1,9 +1,22 @@
 from flask import Blueprint, render_template
-from rfm_dashboard.models.rfm_data import get_rfm_data
+from rfm_dashboard.models.rfm_data import *
 
 bp = Blueprint('landing_page', __name__)
 
-@bp.route('/rfm',methods=['GET'])
+@bp.route('/rfm', methods=['GET'])
 def index():
     rfm_data = get_rfm_data()
+
     return render_template('landing/index.html', rfm_data=rfm_data)
+
+@bp.route('/barchart', methods=['GET'])
+def barchart():
+    rfm_data = get_rfm_data()
+    r_percent = get_r_percent()
+    f_percent = get_f_percent()
+    m_percent = get_m_percent()
+    group_r_percent = [(keys, values) for i, (keys, values) in enumerate(zip(rfm_data.keys(), r_percent))]
+    group_f_percent = [(keys, values) for i, (keys, values) in enumerate(zip(rfm_data.keys(), f_percent))]
+    group_m_percent = [(keys, values) for i, (keys, values) in enumerate(zip(rfm_data.keys(), m_percent))]
+
+    return render_template('landing/barchart.html', group_r_percent=group_r_percent, group_f_percent=group_f_percent, group_m_percent=group_m_percent)
--- rfm_dashboard/models/rfm_data.py
+++ rfm_dashboard/models/rfm_data.py
@@ -3,4 +3,54 @@ import json
 def get_rfm_data():
     with open('rfm_dashboard/db/rfm_data.json') as file:
         data = json.load(file)
+
     return data
+
+def get_data_list():
+    r_data, f_data, m_data = [], [], []
+
+    for k, v in get_rfm_data().items():
+        r_data.append(v['recency'])
+        f_data.append(v['frequency'])
+        m_data.append(v['monetary'])
+
+    return r_data, f_data, m_data
+
+r_data, f_data, m_data = get_data_list()
+
+def get_r_percent():
+    r_percent = []
+    max_recency = int(max(r_data))
+    min_recency = int(min(i for i in r_data if i > 0))
+
+    for i in r_data:
+        if i == 0:
+            r_percent.append(0)
+        else:
+            r_percent.append(round(((max_recency - i) / (max_recency - min_recency)) * 100))
+
+    return r_percent
+
+def get_f_percent():
+    f_percent = []
+    max_frequency = int(max(f_data))
+
+    for i in f_data:
+        if i == 0:
+            f_percent.append(0)
+        else:
+            f_percent.append(round((i / max_frequency) * 100))
+            
+    return f_percent
+
+def get_m_percent():
+    m_percent = []
+    max_monetary = int(max(m_data))
+
+    for i in m_data:
+        if i == 0:
+            m_percent.append(0)
+        else:
+            m_percent.append(round((i / max_monetary) * 100))
+
+    return m_percent
