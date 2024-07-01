a2d5c9e3d5f969b0a4df0f7f5537901c0a10d4d9
NatthapatYang <natthapatyang@gmail.com>
2024-07-01 19:49:33 +0700

    Move fuction in landing_page to models

--- rfm_dashboard/controller/landing_page.py
+++ rfm_dashboard/controller/landing_page.py
@@ -5,12 +5,7 @@ bp = Blueprint('landing_page', __name__)
 
 @bp.route('/rfm', methods=['GET'])
 def index():
-    rfm_data = get_rfm_data()
-    r_percent = get_r_percent()
-    f_percent = get_f_percent()
-    m_percent = get_m_percent()
-    group_r_percent = [(keys, values) for i, (keys, values) in enumerate(zip(rfm_data.keys(), r_percent))]
-    group_f_percent = [(keys, values) for i, (keys, values) in enumerate(zip(rfm_data.keys(), f_percent))]
-    group_m_percent = [(keys, values) for i, (keys, values) in enumerate(zip(rfm_data.keys(), m_percent))]
+    rfm_data = processor.get_rfm_data()
+    group_r_percent, group_f_percent, group_m_percent = processor.group_rfm_percent()
 
-    return render_template('landing/barchart.html', group_r_percent=group_r_percent, group_f_percent=group_f_percent, group_m_percent=group_m_percent)
+    return render_template('landing/barchart.html', rfm_data=rfm_data, group_r_percent=group_r_percent, group_f_percent=group_f_percent, group_m_percent=group_m_percent)
--- rfm_dashboard/models/rfm_data.py
+++ rfm_dashboard/models/rfm_data.py
@@ -1,56 +1,71 @@
 import json
 
-def get_rfm_data():
-    with open('rfm_dashboard/db/rfm_data.json') as file:
-        data = json.load(file)
+file_path = 'rfm_dashboard/db/rfm_data.json'
 
-    return data
+class RFMDataProcessor:
+    def __init__(self, file_path):
+        self.file_path = file_path
+        self.r_data, self.f_data, self.m_data = self.get_data_list()
 
-def get_data_list():
-    r_data, f_data, m_data = [], [], []
+    def get_rfm_data(self):
+        with open(self.file_path) as file:
+            data = json.load(file)
+        return data
 
-    for k, v in get_rfm_data().items():
-        r_data.append(v['recency'])
-        f_data.append(v['frequency'])
-        m_data.append(v['monetary'])
+    def get_data_list(self):
+        r_data, f_data, m_data = [], [], []
+        for k, v in self.get_rfm_data().items():
+            r_data.append(v['recency'])
+            f_data.append(v['frequency'])
+            m_data.append(v['monetary'])
+        return r_data, f_data, m_data
 
-    return r_data, f_data, m_data
+    def get_r_percent(self):
+        r_percent = []
+        max_recency = int(max(self.r_data))
+        min_recency = int(min(i for i in self.r_data if i > 0))
 
-r_data, f_data, m_data = get_data_list()
+        for i in self.r_data:
+            if i == 0:
+                r_percent.append(0)
+            else:
+                r_percent.append(round(((max_recency - i) / (max_recency - min_recency)) * 100))
 
-def get_r_percent():
-    r_percent = []
-    max_recency = int(max(r_data))
-    min_recency = int(min(i for i in r_data if i > 0))
+        return r_percent
 
-    for i in r_data:
-        if i == 0:
-            r_percent.append(0)
-        else:
-            r_percent.append(round(((max_recency - i) / (max_recency - min_recency)) * 100))
+    def get_f_percent(self):
+        f_percent = []
+        max_frequency = int(max(self.f_data))
 
-    return r_percent
+        for i in self.f_data:
+            if i == 0:
+                f_percent.append(0)
+            else:
+                f_percent.append(round((i / max_frequency) * 100))
+                
+        return f_percent
 
-def get_f_percent():
-    f_percent = []
-    max_frequency = int(max(f_data))
+    def get_m_percent(self):
+        m_percent = []
+        max_monetary = int(max(self.m_data))
 
-    for i in f_data:
-        if i == 0:
-            f_percent.append(0)
-        else:
-            f_percent.append(round((i / max_frequency) * 100))
-            
-    return f_percent
+        for i in self.m_data:
+            if i == 0:
+                m_percent.append(0)
+            else:
+                m_percent.append(round((i / max_monetary) * 100))
 
-def get_m_percent():
-    m_percent = []
-    max_monetary = int(max(m_data))
+        return m_percent
 
-    for i in m_data:
-        if i == 0:
-            m_percent.append(0)
-        else:
-            m_percent.append(round((i / max_monetary) * 100))
+    def group_rfm_percent(self):
+        r_percent = self.get_r_percent()
+        f_percent = self.get_f_percent()
+        m_percent = self.get_m_percent()
+        group_r_percent = [(keys, values) for keys, values in zip(self.get_rfm_data().keys(), r_percent)]
+        group_f_percent = [(keys, values) for keys, values in zip(self.get_rfm_data().keys(), f_percent)]
+        group_m_percent = [(keys, values) for keys, values in zip(self.get_rfm_data().keys(), m_percent)]
 
-    return m_percent
+        return group_r_percent, group_f_percent, group_m_percent
+
+processor = RFMDataProcessor(file_path)
+group_r_percent, group_f_percent, group_m_percent = processor.group_rfm_percent()
